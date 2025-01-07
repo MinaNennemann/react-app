b2a4a04eb1db171fc529a3ecff11db866ae83817
redithomas <redi@msstl.de>
2025-01-07 20:24:50 +0100

    useState

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   const [selectedIndex, setSelectedIndex] = useState(-1);
