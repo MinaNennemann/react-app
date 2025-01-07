3c05b6ac77469bab5e3ecdce9bc379dc6e20f166
redithomas <redi@msstl.de>
2025-01-07 20:24:36 +0100

    useState

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,18 +1,10 @@
-
-function Message(){
-
-
-const name = '';
-if (name) {
+function Message() {
+  const name = "Mina";
+  if (name) {
     return <h1>Hello {name} </h1>;
+  }
 
-
-}
-
-return <h1>Hello Guest</h1>;
-
-
+  return <h1>Hello Guest</h1>;
 }
 
 export default Message;
-
